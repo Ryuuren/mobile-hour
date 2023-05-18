d6880e3132c6da46423af69f1fbb46020893c3cf
Mitchell Patch <111026568+Ryuuren@users.noreply.github.com>
2023-05-18 19:01:30 +1000

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,13 @@
 import mysql from "mysql2/promise";
+import dotenv from "dotenv"
 
-export const db_conn = mysql.createPool({
-  host: "localhost",
-  port: 3307,
-  user: "root",
-  password: "root",
-  database: "mobile-hour",
+// Load environment variables from .env file
+dotenv.config();
+
+export const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
